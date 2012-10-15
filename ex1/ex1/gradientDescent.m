647d393804e852de14337734cb6e1efa1e7d8384
erogol <erogol@erogol-G50V.(none)>
2012-10-16 02:23:43 +0300

    initial push

---
+++ ex1/ex1/gradientDescent.m
@@ -0,0 +1,40 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+    
+    z = (X*theta)-y;
+    z = sum(z);
+    z = z * (1/m);
+    z = z * alpha;
+    
+    z2 = ((X*theta)-y).*X(:,2);
+    z2 = sum(z2);
+    z2 = z2 * (1/m);
+    z2 = z2 * alpha;
+    
+    theta(1,1) = theta(1,1) - z;
+    theta(2,1) = theta(2,1) - z2;
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
---
+++ ex4/ex4_kaggle/read_data.m
@@ -0,0 +1,2 @@
+train_data = csvread('train.csv',1,0);
+test_data = csvread('test.csv',1,0);
